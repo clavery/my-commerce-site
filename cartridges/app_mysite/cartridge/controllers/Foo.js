ff136b20bc9f93db2434f9aae7b05630e0ec9450
Charles Lavery <clavery@salesforce.com>
2025-07-17 14:17:30 -0400

    initial

---
+++ cartridges/app_mysite/cartridge/controllers/Foo.js
@@ -0,0 +1,11 @@
+const server = require("server");
+
+
+server.get("Bar", function (req, res, next) {
+  res.json({
+    message: "Hello from Bar endpoint",
+  });
+  next();
+});
+
+module.exports = server.exports();
