1d8ddd965b4d4187f5da11bc855350cb6b70c49c
Charles Lavery <clavery@salesforce.com>
2025-07-17 14:18:06 -0400

    Squashed 'dependencies/b2c-tools/' content from commit 3071624
    git-subtree-dir: dependencies/b2c-tools
    git-subtree-split: 3071624d97f1b5e0962a32883b0b9ccb2863b368

---
+++ types/command-mcp.d.ts
@@ -0,0 +1,5 @@
+export let command: string;
+export let desc: string;
+export function builder(yargs: any): any;
+export function handler(): Promise<void>;
+//# sourceMappingURL=command-mcp.d.ts.map
